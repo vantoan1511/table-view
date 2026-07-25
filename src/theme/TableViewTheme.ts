63c0a3eaa9369dd250664271f3c81ad0f3a7120d
Toan Nguyen Van <vantoan1517@gmail.com>
2026-07-25 16:10:55 +0700

    feat: improve components style

--- src/theme/TableViewTheme.ts
+++ src/theme/TableViewTheme.ts
@@ -43,17 +43,57 @@ export const TableViewTheme = definePreset(Aura, {
       dark: {
         surface: {
           0: '#ffffff',
-          50: '{slate.50}',
-          100: '{slate.100}',
-          200: '{slate.200}',
-          300: '{slate.300}',
-          400: '{slate.400}',
-          500: '{slate.500}',
-          600: '{slate.600}',
-          700: '{slate.700}',
-          800: '{slate.800}',
-          900: '{slate.900}',
-          950: '{slate.950}'
+          50: '{gray.50}',
+          100: '{gray.100}',
+          200: '{gray.200}',
+          300: '{gray.300}',
+          400: '{gray.400}',
+          500: '{gray.500}',
+          600: '{gray.600}',
+          700: '{gray.700}',
+          800: '{gray.800}',
+          900: '{gray.900}',
+          950: '{gray.950}'
+        }
+      }
+    }
+  },
+  components: {
+    button: {
+      colorScheme: {
+        dark: {
+          text: {
+            secondary: {
+              hoverBackground: 'var(--color-primary-light)',
+              activeBackground: 'var(--color-primary-light)'
+            }
+          },
+          root: {
+            primary: {
+              activeBackground: 'var(--color-primary-hover)',
+              hoverBackground: 'var(--color-primary-hover)',
+              hoverBorderColor: 'var(--color-border-strong)',
+              activeBorderColor: 'var(--color-border-strong)'
+            }
+          }
+        },
+        light: {
+          text: {
+            secondary: {
+              hoverBackground: '{slate.100}',
+              activeBackground: '{slate.200}'
+            }
+          }
+        }
+      }
+    },
+    toggleswitch: {
+      colorScheme: {
+        dark: {
+          root: {
+            background: 'var(--color-primary-light)',
+            hoverBackground: 'var(--color-primary-light)'
+          }
         }
       }
     }
